848eedcb0d7acd65135b20001d2bf704d3ccfdf5
Sirothpech <christophe.ngan@gmail.com>
2024-01-23 16:00:14 +0100

    Add prime number calculation and performance measurement code

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,24 @@
+function countPrimeNumbers() {
+    let array = [];
+    for (let i = 2; i <= 100; i++) {
+        let flag = false;
+        for (let j = 2; j <= i; j++) {
+            if (i % j === 0 && i !== j) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag === false) {
+            array.push(i);
+        }
+    }
+    return array.length;
+}
+
+const start = performance.now();
+for (let i = 0; i <= 100; i++) {
+    countPrimeNumbers();
+}
+const end = performance.now();
+const executionTime = end - start;
+console.log(`Execution time of calculating prime numbers 100 times was ${executionTime} milliseconds.`);
